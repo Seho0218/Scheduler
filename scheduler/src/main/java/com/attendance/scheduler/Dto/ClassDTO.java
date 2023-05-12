ec071963af0aed6c8b08af4be7d41e1dc1f23031
Seho0218 <dev.hsayho@gmail.com>
2023-05-13 00:42:43 +0900

    classDTO,NotEmpty

--- scheduler/src/main/java/com/attendance/scheduler/Dto/ClassDTO.java
+++ scheduler/src/main/java/com/attendance/scheduler/Dto/ClassDTO.java
@@ -1,6 +1,7 @@
 package com.attendance.scheduler.Dto;
 
 import com.attendance.scheduler.Entity.ClassEntity;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 
 import java.sql.Timestamp;
@@ -11,16 +12,26 @@ import java.sql.Timestamp;
 public class ClassDTO {
 
     //    학생 이름
+    @NotEmpty
     private String studentName;
     //    수업 횟수
     private int counts;
 
     //    수업 시간
-    private int Monday;
-    private int Tuesday;
-    private int Wednesday;
-    private int Thursday;
-    private int Friday;
+    @NotEmpty
+    private Integer Monday;
+
+    @NotEmpty
+    private Integer Tuesday;
+
+    @NotEmpty
+    private Integer Wednesday;
+
+    @NotEmpty
+    private Integer Thursday;
+
+    @NotEmpty
+    private Integer Friday;
 
     private Timestamp updateTimeStamp;
 
