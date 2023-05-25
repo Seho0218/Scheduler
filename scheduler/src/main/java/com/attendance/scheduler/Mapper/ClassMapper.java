031ce8a4fd4fadebfb431b6fc5a25944deb44d78
Seho0218 <dev.hsayho@gmail.com>
2023-05-26 03:28:10 +0900

    search_class, view_submitted_class, modify_classes

--- scheduler/src/main/java/com/attendance/scheduler/Mapper/ClassMapper.java
+++ scheduler/src/main/java/com/attendance/scheduler/Mapper/ClassMapper.java
@@ -3,8 +3,12 @@ package com.attendance.scheduler.Mapper;
 import com.attendance.scheduler.Dto.ClassDTO;
 import com.attendance.scheduler.Entity.ClassEntity;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
 @Mapper(componentModel = "spring")
+@Component
 public interface ClassMapper {
     ClassDTO toClassDTO(ClassEntity classEntity);
 }
+
+
--- scheduler/src/main/java/com/attendance/scheduler/Mapper/StudentClassMapper.java
+++ scheduler/src/main/java/com/attendance/scheduler/Mapper/StudentClassMapper.java
@@ -3,8 +3,10 @@ package com.attendance.scheduler.Mapper;
 import com.attendance.scheduler.Dto.StudentClassDTO;
 import com.attendance.scheduler.Entity.ClassEntity;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
 @Mapper(componentModel = "spring")
+@Component
 public interface StudentClassMapper {
     StudentClassDTO toClassDTO(ClassEntity classEntity);
 }
